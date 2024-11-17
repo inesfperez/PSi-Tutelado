c0e34123cef96f540a3ce58c75c2948a0fcfed82
Ines <ines.fperez@udc.es>
2024-11-17 11:34:53 +0100

    Login semi-funcional, falta implementar opción de registro

--- CareSafe/app/src/main/java/es/udc/psi/caresafe/LoginActivity.java
+++ CareSafe/app/src/main/java/es/udc/psi/caresafe/LoginActivity.java
@@ -3,6 +3,8 @@ package es.udc.psi.caresafe;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -11,16 +13,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final String REMEMBER_KEY = "rememberMe";
+    private static final String SHARED_PREFS_KEY = "CareSafePrefs";
 
     private EditText emailEditText;
     private EditText passwordEditText;
@@ -44,10 +53,10 @@ public class LoginActivity extends AppCompatActivity {
 
         // Inicializar Firebase y SharedPreferences
         mAuth = FirebaseAuth.getInstance();
-        sharedPreferences = getSharedPreferences("CareSafePrefs", MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE);
 
         // Verificamos si hay sesión activa, si la hay vamos directamente a la app
-        if (sharedPreferences.getBoolean("rememberMe", false)) {
+        if (sharedPreferences.getBoolean(REMEMBER_KEY, false)) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
@@ -65,6 +74,7 @@ public class LoginActivity extends AppCompatActivity {
         rememberMeCheckBox = findViewById(R.id.remember_me_cb);
         loginButton = findViewById(R.id.login_btn);
         registerTextView = findViewById(R.id.register_tv);
+        progressBar = findViewById(R.id.progress_bar_login);
 
         loginButton.setOnClickListener(v -> {
             String email = emailEditText.getText().toString();
@@ -72,10 +82,39 @@ public class LoginActivity extends AppCompatActivity {
 
             // Comprobamos que los campos no estén vacíos
             if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(LoginActivity.this, R.string.empty_emailOrPassw_msg, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, R.string.empty_emailOrPassw_msg, Toast.LENGTH_SHORT).show();
+                return;
             }
 
             // Mostramos el ProgressBar
+            progressBar.setVisibility(ProgressBar.VISIBLE);
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                // Inicio de sesión exitoso
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if (user != null){
+                                    user.sendEmailVerification();
+                                    if (rememberMeCheckBox.isChecked()) {
+                                        sharedPreferences.edit().putBoolean(REMEMBER_KEY, true).apply();
+                                    }
+                                    Toast.makeText(LoginActivity.this, getText(R.string.succesful_login), Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            } else {
+                                // Error en inicio de sesión
+                                Log.e(TAG, "Error en inicio de sesión: " + task.getException());
+                                Toast.makeText(LoginActivity.this, getText(R.string.error_authentication), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
         });
     }
 }
