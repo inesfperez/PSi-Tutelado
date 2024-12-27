6114de5985f64ee3dd16003b05b06ab2777886dd
carlosAlvarez2 <91893995+carlosAlvarez2@users.noreply.github.com>
2024-12-27 18:24:00 +0100

    Cierre de Sesion
    Añadida funcionalidad de cierre de sesión desde la actividad principal

--- CareSafe/app/src/main/java/es/udc/psi/caresafe/MainActivity.java
+++ CareSafe/app/src/main/java/es/udc/psi/caresafe/MainActivity.java
@@ -1,6 +1,9 @@
 package es.udc.psi.caresafe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        logout();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Si el usuario no está autenticado, redirige a LoginActivity
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public void logout() {
+        logoutButton = findViewById(R.id.logOut_btn);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut(); // Cierra la sesión
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Limpia el stack de actividades
+                startActivity(intent);
+                finish(); // Finaliza la actividad actual
+            }
+        });
+
     }
 }
