8978cdc99e8dd0c14e00a33efe4cc2139174be08
carlosAlvarez2 <91893995+carlosAlvarez2@users.noreply.github.com>
2025-01-20 19:47:16 +0100

    Merge branch 'refs/heads/feature-email-notifier' into develop
    # Conflicts:
    #	.idea/workspace.xml
    #	CareSafe/app/build.gradle.kts
    #	CareSafe/app/src/main/AndroidManifest.xml
    #	CareSafe/app/src/main/java/es/udc/psi/caresafe/NotifyMainActivity.java
    #	CareSafe/app/src/main/res/values/strings.xml

--- CareSafe/app/src/main/java/es/udc/psi/caresafe/MainActivity.java
+++ CareSafe/app/src/main/java/es/udc/psi/caresafe/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import es.udc.psi.caresafe.databinding.ActivityLoginBinding;
 import es.udc.psi.caresafe.databinding.ActivityMainBinding;
-import es.udc.psi.caresafe.FallDetection.FallDetectionService;
 import es.udc.psi.caresafe.FallDetection.serviceFallDetecManager;
 
 public class MainActivity extends AppCompatActivity {
